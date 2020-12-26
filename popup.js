cad31335db99efeda2e32bfbf44a42461c2e7ccc
kodcanlisi <developerindiee@gmail.com>
2020-12-26 15:03:38 +0300

    Check the input if blank

--- popup.js
+++ popup.js
@@ -1,12 +1,19 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const checkPageButton = document.getElementById('clickIt');
-    const input = document.getElementById('word');
-    
-    checkPageButton.addEventListener('click', function(){
-        chrome.tabs.getSelected(null, function(tab) {
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {word: input.value});
-            });
-        })
-    })
+document.addEventListener("DOMContentLoaded", function () {
+  const checkPageButton = document.getElementById("clickIt");
+  const input = document.getElementById("word");
+
+  checkPageButton.addEventListener("click", function () {
+    if (input.value === "") {
+        input.style.border="2px solid red";
+    } else {
+      chrome.tabs.getSelected(null, function (tab) {
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { word: input.value });
+          }
+        );
+      });
+    }
+  });
 });
